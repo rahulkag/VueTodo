395f6399344eca0ad082fb2a8a215cce6c19a58e
Rahul Kag <kag.rahul@gmail.com>
2024-01-15 18:14:23 +0530

    updated validation

--- src/components/TodoList/AddTodo.vue
+++ src/components/TodoList/AddTodo.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-form @submit.prevent="addTodo">
+    <v-form @submit.prevent="addTodo" ref="addItemForm">
         <v-row>
             <v-col cols="8">
                 <v-text-field name="addItem" v-model="itemText" :rules="rules" label="Enter item"
                     autocomplete="off"></v-text-field>
             </v-col>
             <v-col cols="4">
-                <v-btn type="submit" block rounded="0" size="large" prepend-icon="mdi-plus" color="btnPrimary">
+                <v-btn type="submit" block rounded="0" size="x-large" prepend-icon="mdi-plus" color="btnPrimary">
                     Add Item
                 </v-btn>
             </v-col>
@@ -20,12 +20,12 @@ export default {
     data() {
         return {
             itemText: '',
-            // rules: [
-            //     value => {
-            //         if (value) return true
-            //         return 'You must enter a item.'
-            //     },
-            // ],
+            rules: [
+                value => {
+                    if (value) return true
+                    return 'You must enter a item.'
+                },
+            ],
         }
     },
     methods: {
@@ -34,6 +34,7 @@ export default {
                 this.$emit("add-todo", this.itemText);
                 this.itemText = '';
                 this.snackbar = true
+                this.$refs.addItemForm.reset();
             }
         }
     }
